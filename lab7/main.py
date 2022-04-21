97f0ef5e9538b2d0519479ab5e4d8ce3e726b37a
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2022-04-21 16:02:41 +0200

    Add 7th list

---
+++ lab7/main.py
@@ -0,0 +1,13 @@
+import tasks
+
+
+def main():
+    # tasks.task_1()
+    # tasks.task_2()
+    # tasks.task_3()
+    # tasks.task_4()
+    tasks.task_5()
+
+
+if __name__ == '__main__':
+    main()
